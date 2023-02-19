2025d1624dc8a339e15bccb84976f18637750c5e
myuser <myuser@localhost>
2023-02-18 21:04:22 -0600

    Added implementation

--- oops-2/src/com/jarvis/exceptionhandling/ExceptionHandlingRunner.java
+++ oops-2/src/com/jarvis/exceptionhandling/ExceptionHandlingRunner.java
@@ -1,8 +1,50 @@
 package com.jarvis.exceptionhandling;
 
+class Amount {
+
+	private String currency;
+	private int amount;
+
+	public Amount(String currency, int amount) {
+		this.currency = currency;
+		this.amount = amount;
+	}
+
+	public void add(Amount that) throws CurrenciesDonotMatchException {
+
+		if (!this.currency.equals(that.currency)) {
+
+			// System.out.println("Currencies donot match\n");
+			throw new CurrenciesDonotMatchException("Currencies donot match\n");
+		}
+
+		this.amount = this.amount + that.amount;
+
+	}
+
+	@Override
+	public String toString() {
+		return amount + " " + currency;
+	}
+}
+
+class CurrenciesDonotMatchException extends Exception {
+
+	public CurrenciesDonotMatchException(String msg) {
+		super(msg);
+	}
+
+}
+
 public class ExceptionHandlingRunner {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws CurrenciesDonotMatchException {
+
+		Amount amount1 = new Amount("USD", 10);
+		Amount amount2 = new Amount("EUR", 10);
+
+		amount1.add(amount2);
+		System.out.println(amount1);
 
 	}
 
